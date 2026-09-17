787a1ab4b97023464d4730fd853d3377ab9402d4
Arif Nur Rohman <70461314+arifnurrhmnn@users.noreply.github.com>
2026-09-17 15:56:03 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,7 @@ export default function RootLayout({
 
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3919107586910983"
      crossorigin="anonymous"></script>
+
       </head>
       <body className="antialiased font-sans">
         {children}
